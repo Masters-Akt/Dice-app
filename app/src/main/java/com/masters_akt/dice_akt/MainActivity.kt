40ead44c3cae16135c71d01f41ff446166cbf51c
Masters-Akt <kumar.ankit.jan01@gmail.com>
2020-05-02 02:15:14 +0530

    Basic layout

---
+++ app/src/main/java/com/masters_akt/dice_akt/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.masters_akt.dice_akt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onButtonClick(view: View) {
+        val randomNumber = (1..6).random()
+        textView2.text=randomNumber.toString()
+    }
+}
